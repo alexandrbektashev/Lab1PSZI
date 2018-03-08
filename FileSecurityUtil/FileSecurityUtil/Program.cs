9bb8af131b19164e6b68e5c4cf9bfb1ea30fe8b7
alexandrbektashev <alexandr.bektashev@gmail.com>
2018-03-08 22:49:12 +0300

    nothing done:) fuck this shit:)
    oh uzh eto programmirovanie

---
+++ FileSecurityUtil/FileSecurityUtil/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices; //Подключаем библиотеку для импорта и работы с WinApi
+
+namespace FileSecurityUtil
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            IntPtr ptr = WinAPI.FindWindow(null, "Документы");
+            Console.WriteLine(ptr.ToString());
+            IntPtr child;
+            StringBuilder title = new StringBuilder();
+            if (ptr.ToInt32() != 0)
+            {
+                child = WinAPI.GetWindow(ptr, WinAPI.GetWindow_Cmd.GW_CHILD);
+                Console.WriteLine(child.ToString());
+                WinAPI.SendMessage(child, Convert.ToInt32(WinAPI.GetWindow_Cmd.WM_GETTEXT), (IntPtr)20, title);
+                Console.WriteLine(title.ToString());
+            }
+
+            string fn = Console.ReadLine();
+            Security.SetFileOrFolderOwner(fn);
+
+            Console.Read();
+            return 0;
+        }
+
+        
+    }
+
+
+   
+}
+
